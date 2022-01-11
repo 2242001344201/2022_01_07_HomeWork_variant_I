5ef1fad9c954cc3187341cb5625f4e4a5f9bdde0
224200134401 <eroshev.aleksandr@mail.ru>
2022-01-11 14:52:07 +0300

    commit 2

--- src/homework/BubbleSort.java
+++ src/homework/BubbleSort.java
@@ -2,7 +2,6 @@ package homework;
 
 public class BubbleSort extends SortingContext {
     public BubbleSort() {
-        //System.out.println("BubbleSort");
         this.sorter = new BubbleSortPerformance();
     }
 }
